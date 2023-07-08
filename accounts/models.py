9aa79bb1c5e2701aee4fd0d83b15f4e573b2ba35
Abdusattor <abdusattor1999@gmail.com>
2023-07-08 20:48:54 +0500

    Modellar o'zgartirildi

--- accounts/models.py
+++ accounts/models.py
@@ -5,11 +5,10 @@ from autoslug import AutoSlugField
 
 class User(AbstractBaseUser,PermissionsMixin):
     phone = models.CharField(max_length=64, verbose_name="Telefon", unique=True)
-    activated_date = models.DateTimeField(blank=True, null=True, verbose_name='Activlashgan Vaqti')
-    is_seller = models.BooleanField(default=False, verbose_name="Sotuvchi holati")
     is_active = models.BooleanField(default=False, verbose_name='Activligi')
     is_staff = models.BooleanField(default=False)
     is_superuser = models.BooleanField(default=False)
+    created_at = models.DateTimeField(blank=True, null=True, verbose_name='Activlashgan Vaqti')
 
     USERNAME_FIELD = 'phone'
     REQUIRED_FIELDS = []
@@ -24,7 +23,7 @@ class User(AbstractBaseUser,PermissionsMixin):
 
 class Kitchen(models.Model):
     name = models.CharField(max_length=50, verbose_name="Oshxona nomi")
-    slug = AutoSlugField(populate_from=name)
+    slug = AutoSlugField(populate_from='name')
     user = models.ForeignKey(User, on_delete=models.CASCADE, verbose_name='Foydalanuchi')
     image = models.ImageField(upload_to='images/kitchen-images/', verbose_name='Oshxona Logosi')
     description = models.TextField(verbose_name='Oshxona haqida malumot')
@@ -49,11 +48,12 @@ class Position(models.Model):
 class Worker(User):
     name = models.CharField(max_length=30, verbose_name='Ism')
     surname = models.CharField(max_length=30, verbose_name='Familiya')
-    slug = AutoSlugField(populate_from = (name, surname))
+    slug = AutoSlugField(populate_from='name', unique_with=('oshxona', 'created'))
     salary = models.DecimalField(max_digits=9, decimal_places=2, verbose_name='Maosh')
     oshxona = models.ForeignKey(Kitchen, on_delete=models.CASCADE, verbose_name='Oshxona')
     position = models.ForeignKey(Position , on_delete=models.SET_NULL, null=True, verbose_name="Lavozimi")
     percentage = models.PositiveIntegerField(blank=True, null=True, verbose_name="Ulush Foizda (ixtiyoriy)")
+    created = models.DateTimeField(auto_now_add=True, verbose_name="Qo'shilgan vaqti")
 
     def __str__(self):
         return f"{self.name} {self.surname}".title()
